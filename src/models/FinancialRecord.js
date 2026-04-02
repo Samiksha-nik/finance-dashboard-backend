0d8726126789fcf2d3263d3cb8a7af9ea88ec818
Samiksha-nik <nikamsamiksha43@gmail.com>
2026-04-02 13:27:20 +0530

    Add FinancialRecord model

---
+++ src/models/FinancialRecord.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const financialRecordSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    amount: { type: Number, required: true, min: 0 },
+    type: { type: String, enum: ["income", "expense"], required: true },
+    category: { type: String, required: true, trim: true },
+    date: { type: Date, required: true },
+    note: { type: String, default: "", trim: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("FinancialRecord", financialRecordSchema);
+
--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const User = require("./User");
+const FinancialRecord = require("./FinancialRecord");
 
-module.exports = { User };
+module.exports = { User, FinancialRecord };
 
